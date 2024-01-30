3900123a1219164066e925d7aa0e9950091105fb
Decay <decayking9@gmail.com>
2024-01-30 16:23:57 +0100

    ajout du auteur.java

---
+++ Auteur.java
@@ -0,0 +1,26 @@
+public class Auteur{
+
+    private String nom;
+    private int perfTrage;
+    private String citTrage;
+    private int perfCome;
+    private String citCome;
+    private int perfDrame;
+    private String citDrame;
+
+    public Auteur (nom, perfTrage, citTrage, perfCome, citCome, perfDrame, citDrame){
+        this.nom = nom;
+        this.perfTrage = perfTrage;
+        this.citTrage = citTrage;
+        this.perfCome = perfCome;
+        this.citCome = citCome;
+        this.perfDrame = perfDrame;
+        this.citDrame = citDrame;
+    }
+
+    public void getQualitéTragédie(){
+        return this.perfTrage;
+    }
+
+    
+}
