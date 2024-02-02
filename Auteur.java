c4dee4aaf0255b8339ec1444fe36a4444223b69c
Decay <decayking9@gmail.com>
2024-02-02 14:49:18 +0100

    ajout de plein de trucs

--- Auteur.java
+++ Auteur.java
@@ -26,8 +26,60 @@ public class Auteur{
         return this.citTrage;
     }
 
+    public int getQualitéComédie(){
+        return this.perfCome;
+    }
+
+    public String getCitationComédie(){
+        return this.citCome;
+    }
+
+    public int getQualitéDrame(){
+        return this.perfDrame;
+    }
+
+    public String getCitationDrame(){
+        return this.citDrame;
+    }
+
+    public String pointfort(){
+         if (perfTrage >= perfCome && perfTrage >= perfDrame) {
+        return "Tragedy";
+    } else if (perfCome >= perfTrage && perfCome >= perfDrame) {
+        return "Comedy";
+    } else {
+        return "Drama";
+    }
+    }
+
+    public int qualitéStyle(Style s){
+          switch (s) {
+        case TRAGEDIE:
+            return perfTrage;
+        case COMEDIE:
+            return perfCome;
+        case DRAMA:
+            return perfDrame;
+        default:
+            return 0
+        }
+    }
+
+     public int citationStyle(Style s){
+        switch (s) {
+        case TRAGEDIE:
+            return "citation Tragedie: " + citTrage;
+        case COMEDIE:
+            return "citation Comédie: " + citCome;
+        case DRAMA:
+            return "citation Drama: " + citDrame;
+        default:
+            return "";
+    }
+
     @Override
     public String toString(){
         return "L'honnorable" + nom;
     }
+}
 }
